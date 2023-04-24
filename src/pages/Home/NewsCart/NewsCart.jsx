bb357c254e075e6656bb614c2105d46fd71b5986
Saiful264 <saifulislamx264@gmail.com>
2023-04-24 12:24:33 +0600

    news and category part all complete

--- src/pages/Home/NewsCart/NewsCart.jsx
+++ src/pages/Home/NewsCart/NewsCart.jsx
@@ -1,22 +1,66 @@
+import moment from "moment/moment";
 import React from "react";
+import Rating from "react-rating";
 import { Card, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from "react-icons/fa";
 
 const NewsCart = ({ news }) => {
-  const {_id, title, details,image_url, author} = news;
+  const { _id, title, details, image_url, author, total_view, rating } = news;
   return (
     <Card className="mb-4">
-      <Card.Header>
-      <Image src={author.img} roundedCircle />
+      <Card.Header className="d-flex align-items-center">
+        <Image style={{ height: "40px" }} src={author?.img} roundedCircle />
+        <div className="ps-2 flex-grow-1">
+          <p className="mb-0">{author?.name}</p>
+          <p>
+            <small>
+              {moment(author?.published_date).format("D-MM-YYYY")}
+              {}
+            </small>
+          </p>
+        </div>
+        <dir>
+          <FaRegBookmark></FaRegBookmark>
+          <FaShareAlt></FaShareAlt>
+        </dir>
       </Card.Header>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Img variant="top" src={image_url} />
         <Card.Text>
-          {details.length < 250 ? <>{details}</> : <>{details.slice(0, 240)}..<Link to={`/news/${_id}`}>Read More</Link></>}
+          {details.length < 250 ? (
+            <>{details}</>
+          ) : (
+            <>
+              {details.slice(0, 240)}..
+              <Link to={`/news/${_id}`}>Read More</Link>
+            </>
+          )}
         </Card.Text>
       </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      <Card.Footer className="text-muted d-flex">
+        <div className="flex-grow-1">
+          <Rating
+            placeholderRating={rating.number}
+            readonly
+            emptySymbol={
+              <FaRegStar></FaRegStar>
+            }
+            placeholderSymbol={
+              <FaStar className="text-warning"></FaStar>
+            }
+            fullSymbol={
+              <FaStar></FaStar>
+            }
+          ></Rating>
+          <span>{rating?.number}</span>
+        </div>
+        <div>
+          <FaEye></FaEye>
+          {total_view}
+        </div>
+      </Card.Footer>
     </Card>
   );
 };
--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { Link, useLoaderData } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
+import EditorInsights from "../EditorInsights/EditorInsights";
 
 const News = () => {
-    return (
-        <div>
-            <h2>The news is coming soon</h2>
-        </div>
-    );
+  const news = useLoaderData();
+  const { title, details, image_url, category_id } = news;
+  return (
+    <div>
+      <Card>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{details}</Card.Text>
+          <Link to={`/category/${category_id}`}>
+            <Button variant="danger">
+              {" "}
+              <FaArrowLeft></FaArrowLeft> All The news in this Category
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
+      <EditorInsights/>
+    </div>
+  );
 };
 
 export default News;
